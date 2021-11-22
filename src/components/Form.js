f3a35c737074f921166019b54bebe46b3cf3aac2
mattg1243 <mattgallucci97@gmail.com>
2021-11-22 15:24:46 -0800

    Recipes component improved, also storing may data from the API call

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Form = (props) => (
-    <form onSubmit={props.getRecipes}>
-        <input type="text" name="recipeName"></input>
-        <button type="submit">Search</button>
+    <form onSubmit={props.getRecipes} style={{ marginBottom:"2rem" }}>
+        <input className="form__input" type="text" name="recipeName"></input>
+        <button className="form__button" type="submit">Search</button>
     </form>
 )
 
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 const Recipes = props => (
-    <div>
-        {props.recipes.map((recipe) =>{
-            return <>
-                <div key={recipe.id}>
-                    <img src={recipe.image}/>
-                    <p>{recipe.title}</p>
-                </div>
-            </>
-            
-        })}
-    </div>  
+    <div className="container">
+        <div className="row">
+            {props.recipes.map((recipe) =>{
+                return (
+                    <div className="col-md-4">
+                        <div className="recipes__box">
+                            <div key={recipe.id}>
+                                <img className="recipes__box-img" src={recipe.image} alt={`${recipe.title}`}/>
+                                <div className="recipes__text">
+                                    <h5 className="recipes__title">{ recipe.title.length < 20 ? `${recipe.title}`: 
+                                        `${recipe.title.substring(0, 25)}...`  }</h5>
+                                    <p className="recipes__subtitle">Source : <span> { recipe.sourceName } </span></p>
+                                </div>
+                                <button className="recipe_buttons">View Recipe</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })} 
+        </div>
+    </div>
+    
 );
 
 export default Recipes;
